b272261c491a652e02d644faf7a28f319b5313f9
Jost Hren <jost.hren@getinge.com>
2024-12-06 13:44:24 +0100

    feat(fe): add debounce to search input

--- src/components/SearchInput/SearchInput.tsx
+++ src/components/SearchInput/SearchInput.tsx
@@ -1,9 +1,11 @@
-import { useState } from "react";
+import { useMemo, useState } from "react";
+import { debounce } from "lodash-es";
 import { useSearchParams } from "react-router";
 import { useDispatch } from "react-redux";
 import { updateSeriesQueryName } from "../../store/series/seriesSlice";
 
 const SEARCH_INPUT_PLACEHOLDER = "Search Here!";
+const DEBOUNCE_TIME = 600;
 
 export const SearchInput = (): JSX.Element => {
   const [searchParams, setSearchParams] = useSearchParams();
@@ -11,16 +13,23 @@ export const SearchInput = (): JSX.Element => {
   const [internalValue, setInternalValue] = useState(searchValue);
   const dispatch = useDispatch();
 
-  const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+  const handleInputChange = useMemo(
+    () =>
+      debounce((value: string) => {
+        const params = new URLSearchParams();
+        params.set("search", value);
+        setSearchParams(params, {
+          preventScrollReset: true,
+        });
+
+        dispatch(updateSeriesQueryName(value));
+      }, DEBOUNCE_TIME),
+    [dispatch, setSearchParams]
+  );
+
+  const onChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     setInternalValue(event.target.value);
-
-    const params = new URLSearchParams();
-    params.set("search", event.target.value);
-    setSearchParams(params, {
-      preventScrollReset: true,
-    });
-
-    dispatch(updateSeriesQueryName(event.target.value));
+    handleInputChange(event.target.value);
   };
 
   return (
@@ -28,7 +37,7 @@ export const SearchInput = (): JSX.Element => {
       className="mt-10 p-1 border-2"
       type="text"
       value={internalValue}
-      onChange={handleInputChange}
+      onChange={onChange}
       placeholder={SEARCH_INPUT_PLACEHOLDER}
     />
   );
